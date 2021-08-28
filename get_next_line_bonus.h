97662823435878e149e28f55ec72c5ed26d790a4
Bienvenue <bienvenue@iMacdeBienvenue.lan>
2021-08-28 22:31:55 +0200

    rdy for push

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,18 +1,7 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cwastche </var/mail/cwastche>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/26 10:57:42 by cwastche          #+#    #+#             */
-/*   Updated: 2021/08/26 10:58:06 by cwastche         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
+#include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
 
